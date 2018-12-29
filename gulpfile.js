dece53b3b58d59e690ec985f5a442b1d945faa02
Jon Robson <jonandrewrobson@gmail.com>
2018-12-29 11:01:21 -0600

    injected browsersync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,45 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-// var less = require('gulp-scss');
-// var minifyCSS = require('gulp-csso');
-// var concat = require('gulp-concat');
-// var sourcemaps = require('gulp-sourcemaps');
+var gulp        = require('gulp');
+var uglify      = require('gulp-uglify');
+var sass        = require('gulp-sass');
+var plumber     = require('gulp-plumber');
+var browserSync = require('browser-sync').create();
 
-// gulp.task('styles', function(cb){
-//   return gulp.src('css/*.css')
-//     .pipe(uglify())
-//     .pipe(gulp.dest('build/mincss'))
-//     cb
-// });
 
 // Scripts Task
 gulp.task('scripts', function(cb){
   return gulp.src('js/*.js')
+    .pipe(plumber())
     .pipe(uglify())
     .pipe(gulp.dest('build/minjs'))
     cb
 });
 
-// Watch Task
-// Watches js
-function watchFiles() {
-  gulp.watch("js/*.js", gulp.parallel('scripts'));
-}
+// Compile sass into CSS & auto-inject into browsers
 
-// watch
-gulp.task("watch", gulp.parallel(watchFiles));
+gulp.task('styles', function(cb){
+  return gulp.src('scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest('build/css/'))
+    .pipe(browserSync.stream());
+    cb
+});
 
-gulp.task("default", gulp.parallel('scripts', 'watch'));
+// Static Server + watching scss/html files
+gulp.task('serve', function(cb) {
+    browserSync.init({
+        server: "./journal"
+    });
+    cb
+});
+
+gulp.task('watch', function(cb) {
+	gulp.watch("js/*.js", gulp.parallel('scripts'));
+	gulp.watch("scss/*.scss", gulp.parallel('styles'));
+    gulp.watch("journal/*.html").on('change', browserSync.reload);
+    cb
+});
+
+
+// Gulp Default Task
+gulp.task("default", gulp.parallel('scripts', 'styles' , 'watch' , 'serve'));
